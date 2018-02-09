4e5e133d07a1a054c8b73701a731c13438db3b73
smartkarthik <35652317+smartkarthik@users.noreply.github.com>
2018-02-09 16:29:34 +0530

    Create odd number between two intervals.c

---
+++ odd number between two intervals.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int i,a,b;
+    printf("Enter the number");
+    scanf("%d%d",&a,&b);
+for(i=a+i;i<b;i++)
+    if(a%2!=0)
+    {
+        printf("%d",i);
+    }
+    
+}
